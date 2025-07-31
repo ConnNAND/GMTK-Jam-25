b9623f75416ec3e0ff092e106cfb342878de92a8
AdkinsC0211 <98196686+AdkinsC0211@users.noreply.github.com>
2025-07-30 22:16:07 -0400

    created project and basic menu

---
+++ gmtk-jam-2025/Menus/home_menu.gd
@@ -0,0 +1,43 @@
+extends CenterContainer
+
+func _ready() -> void:
+	$MainMenu/Play.grab_focus()
+
+
+func _on_play_pressed() -> void:
+	$MainMenu.visible = false
+	$ModeSelect.visible = true
+	$ModeSelect/LocalGame.grab_focus()
+
+
+func _on_help_pressed() -> void:
+	$MainMenu.visible = false
+	$Help.visible = true
+	$Help/Back.grab_focus()
+
+
+func _on_options_pressed() -> void:
+	$MainMenu.visible = false
+	$Options.visible = true
+	$Options/Volume.grab_focus()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _modeselect_on_back_pressed() -> void:
+	$ModeSelect.visible = false
+	$MainMenu.visible = true
+	$MainMenu/Play.grab_focus()
+
+
+func _help_on_back_pressed() -> void:
+	$Help.visible = false
+	$MainMenu.visible = true
+	$MainMenu/Play.grab_focus()
+
+func _options_on_back_pressed() -> void:
+	$Options.visible = false
+	$MainMenu.visible = true
+	$MainMenu/Play.grab_focus()
