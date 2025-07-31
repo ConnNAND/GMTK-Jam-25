f0ee0232cdf7d9dac2b75ca7104e412a08261a6b
Tsanzevino <zeldafanta@gmail.com>
2025-07-31 16:38:12 -0400

    idk why so many changes

--- gmtk-jam-2025/Camera/follow_camera.gd
+++ gmtk-jam-2025/Camera/follow_camera.gd
@@ -1,12 +1,13 @@
 extends SpringArm3D
 
 @export var target : Node3D
-var extra_height : float = 2.0
-var follow_distance : float = 5.0
-var follow_speed : float = 5.0
+var follow_distance : float = 1.0
+var placement_offset : Vector3 = Vector3()
+var follow_speed : float = 10.0
 func _ready():
 	spring_length = 8.0
 
 func _process(delta):
-	global_position = lerp(global_position, target.global_position + Vector3(target.global_basis.z.x * follow_distance,extra_height,target.global_basis.z.z * follow_distance), delta * follow_speed)
+	var placement_offset : Vector3 = target.global_basis.z * follow_distance
+	global_position = lerp(global_position, target.global_position + placement_offset, delta * follow_speed)
 	look_at(target.position, target.global_basis.y)
--- gmtk-jam-2025/Characters/Rocketeer/rocketeer.gd
+++ gmtk-jam-2025/Characters/Rocketeer/rocketeer.gd
@@ -1,32 +1,66 @@
 extends CharacterBody3D
 
-var player_id:int = 0
-var movement_input:Vector4 = Vector4.ZERO
-var target_velocity:Vector3 = Vector3.ZERO
-var actual_velocity:Vector3 = Vector3.ZERO
-var speed:float
-var acceleration:float
-var turning:float
-var max_speed:float
-var rotation_speed : float = 5
+var player_id : int = 0
+var movement_input : Vector4 = Vector4.ZERO
+var target_velocity : Vector3 = Vector3.ZERO
+var actual_velocity : Vector3 = Vector3.ZERO
+var speed : float = 25
+var acceleration : float
+var turning : float = 5
+var max_speed : float
+var jump_strength : float = 25
+var gravity : float = 10
+var jump_effect : Vector3 = Vector3.ZERO
+var gravity_effect : Vector3 = Vector3.ZERO
+const NATURAL_UP : Vector3 = Vector3.UP
+var up_vec : Vector3 = Vector3.UP
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity()*delta
-	var movement : Vector3 = get_movement()
-	velocity.x = movement.x * 5
-	velocity.z = movement.z * 5
+		gravity_effect += get_gravity() * delta * gravity
+	else:
+		gravity_effect = Vector3.ZERO
+	gravity_effect += jump_effect
+	jump_effect = Vector3.ZERO
+	# Gets the movement as directed by the player
+	var movement : Vector3 = get_player_movement()
+	velocity = movement + gravity_effect
 	move_and_slide()
 	# Takes care of the rotation of the player
 	var forward_dir = -global_basis.z
+	# This line shouldnt be needed since the y component isnt introduced
 	var move_dir = (movement - movement.project(global_basis.y)).normalized()
-	var full_rotation : float = forward_dir.signed_angle_to(move_dir, global_basis.y)
-	rotate(global_basis.y, lerpf(0, full_rotation, delta * rotation_speed))
+	#var move_dir = movement.normalized()
+	if (move_dir != Vector3.ZERO):
+		var full_rotation : float = forward_dir.signed_angle_to(move_dir, global_basis.y)
+		global_basis = global_basis.rotated(global_basis.y,lerpf(0, full_rotation, delta * turning))
+		global_basis = global_basis.orthonormalized()
 
-func get_movement():
+	
+	# Should be able to rotate x basis (pitch) based on player momentum
+	# This should pull rotation away from vertical, 
+	# and return it to vertical when momentum is lost
+	# The movement relative to their "ground"
+	if is_on_floor() and velocity.length()>2:
+		up_vec.x = rotate_toward(up_vec.x, get_floor_normal().x, delta)
+		up_vec.y = rotate_toward(up_vec.y,get_floor_normal().y, delta)
+		up_vec.z = rotate_toward(up_vec.z,get_floor_normal().z, delta)
+	else:
+		up_vec.x = rotate_toward(up_vec.x,Vector3.UP.x, delta)
+		up_vec.y = rotate_toward(up_vec.y,Vector3.UP.y, delta)
+		up_vec.z = rotate_toward(up_vec.z,Vector3.UP.z, delta)
+	up_vec = up_vec.normalized()
+	global_basis.y = up_vec
+	global_basis.x = global_basis.y.cross(global_basis.z)
+	global_basis.z = global_basis.x.cross(global_basis.y)
+	global_basis = global_basis.orthonormalized()
+	up_direction = global_transform.basis.y
+	
+
+func get_player_movement():
 	var forward = movement_input.x - movement_input.w
 	var right = movement_input.y - movement_input.z
-	return (global_basis.z * forward + global_basis.x * right).normalized()
+	return (global_basis.z * forward + global_basis.x * right).normalized() * speed
 
 func _input(event: InputEvent) -> void:
 	if event.device == player_id:
@@ -45,4 +79,4 @@ func _input(event: InputEvent) -> void:
 
 func jump():
 	if is_on_floor():
-		velocity.y = 10
+		jump_effect = global_basis.y * jump_strength
