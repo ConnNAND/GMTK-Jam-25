f4a484a7d677b73f05c71a6fa8070cce3de56cfc
AdkinsC0211 <98196686+ConnNAND@users.noreply.github.com>
2025-08-02 01:54:11 -0400

    redid boost colliders

--- gmtk-jam-2025/Levels/Scripts/boost_panel.gd
+++ gmtk-jam-2025/Levels/Scripts/boost_panel.gd
@@ -4,6 +4,7 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		body.boost_factor += 3
+		print("gayming")
 
 
 func _on_body_exited(body: Node3D) -> void:
