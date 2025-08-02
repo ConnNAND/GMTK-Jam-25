a665116aceb190da3ecadd7a16910b98d6f752fb
Tsanzevino <zeldafanta@gmail.com>
2025-08-01 20:08:53 -0400

    slow your roll bro

--- gmtk-jam-2025/Camera/follow_camera.gd
+++ gmtk-jam-2025/Camera/follow_camera.gd
@@ -13,7 +13,7 @@ func _process(delta):
 		placement_offset = target.global_basis.z * follow_distance + Vector3.UP
 		global_position = lerp(global_position, target.global_position + placement_offset, delta * follow_speed)
 		$Node3D.look_at(target.position, target.global_basis.y)
-		global_rotation.x = lerp_angle(global_rotation.x, $Node3D.global_rotation.x, 50*delta)
-		global_rotation.y = lerp_angle(global_rotation.y, $Node3D.global_rotation.y, 50*delta)
-		global_rotation.z = lerp_angle(global_rotation.z, $Node3D.global_rotation.z, 50*delta)
+		global_rotation.x = lerp_angle(global_rotation.x, $Node3D.global_rotation.x, 15*delta)
+		global_rotation.y = lerp_angle(global_rotation.y, $Node3D.global_rotation.y, 15*delta)
+		global_rotation.z = lerp_angle(global_rotation.z, $Node3D.global_rotation.z, 15*delta)
 		#look_at(target.position, target.global_basis.y)
