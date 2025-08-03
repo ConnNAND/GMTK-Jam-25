9c3e6bf54fb1c20baced31f16835c8b30814b669
AdkinsC0211 <98196686+ConnNAND@users.noreply.github.com>
2025-08-03 01:48:40 -0400

    muddy

--- gmtk-jam-2025/Levels/Scripts/mud.gd
+++ gmtk-jam-2025/Levels/Scripts/mud.gd
@@ -5,6 +5,7 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		body.hinderance += 0.5
+		$AudioStreamPlayer.play()
 
 
 func _on_body_exited(body: Node3D) -> void:
