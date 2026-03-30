8a32ee146560eedff7dcbbc868300068aed55760
AdkinsC0211 <98196686+ConnNAND@users.noreply.github.com>
2026-03-30 02:39:11 -0400

    did a thing so now you turn left sometimes instead of right

--- gmtk-jam-2025/Levels/procedural_track.gd
+++ gmtk-jam-2025/Levels/procedural_track.gd
@@ -3,7 +3,7 @@ extends WorldEnvironment
 
 @export var SEED:String
 @export var track_area_radius = 500
-@export var track_area_height = 75
+@export var track_area_height = 100
 
 
 var newCurve = Curve3D.new()
@@ -14,12 +14,12 @@ func _ready() -> void:
 	if daily:
 		seed(Time.get_date_string_from_system().hash())
 	if SEED==null or SEED=="":
-		seed(randi())
+		seed(Time.get_datetime_string_from_system().hash())
 	else:
 		seed(SEED.hash())
 	
 	var noise = FastNoiseLite.new()
-	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+	noise.noise_type = FastNoiseLite.TYPE_PERLIN
 	
 	
 	var points = [Vector2.ZERO]
@@ -38,18 +38,30 @@ func _ready() -> void:
 	#combine points that are too close together
 	for i:Vector2 in points:
 		for v:Vector2 in points:
-			if i.distance_to(v)<35 and i!=v:
+			if i.distance_to(v)<60 and i!=v:
 				points.erase(v)
 	#order the exterior points
 	points = Geometry2D.convex_hull(points)
 	points[0].y -= 10
 	points[points.size()-1].y += 10
+	
+	#add more curves between longer stretches
+	for i in range(points.size()):
+		var temp = points[i].distance_to(points[wrapi(i+1, 0, points.size()-1)])
+		if temp>128:
+			var temp2 = (points[i]+points[wrapi(i+1, 0, points.size()-1)])/2
+			temp2.x += randf_range(-temp/4, temp/4)
+			temp2.y += randf_range(-temp/4, temp/4)
+			points.insert(wrapi(i+1, 0, points.size()-1), temp2)
+			i += 1
+	
 	#erase duplicate points
 	var newpoints = []
 	for i in points:
 		if !newpoints.has(i):
-			newpoints.append(Vector3(i.x, (noise.get_noise_2dv(i)-0.5)*track_area_height, i.y))
+			newpoints.append(Vector3(i.x, noise.get_noise_2dv(i)*track_area_height, i.y))
 	points = newpoints
+	
 	for i in range(points.size()):
 		var curPoint:Vector3 = points[i]
 		var nextPoint:Vector3 = points[wrapi(i+1, 0, points.size())]
